36d24f70119b6b160b2d1aabd4f72f907ba5187a
Kamal RD <imkamaljay@gmail.com>
2024-10-07 12:54:17 -0400

    Route.ts Addition
    - This file handles the request to mailchimp
    
    Note: While this works for the time being, I would really like to figure out a way to structure this better as method called POST would not function correctly if we had multiple POST requests

---
+++ f1gs/src/app/api/mailchimp/route.ts
@@ -0,0 +1,55 @@
+import { NextResponse } from "next/server";
+
+import mailchimp from '@mailchimp/mailchimp_marketing';
+import { FormValues } from "@/components/Form";
+
+mailchimp.setConfig({
+    apiKey: process.env.MAILCHIMP_API_KEY,
+    server: process.env.MAILCHIMP_SERVER_PREFIX
+});
+
+const yearToTag: Partial<Record<"1L" | "2L" | "3L", string>> = {
+    "1L": "Class of 2027",
+    "2L": "Class of 2026",
+    "3L": "Class of 2025",
+};
+
+export async function POST(requestDetails: Request) {
+    const body = await requestDetails.json();
+    const formData: FormValues = body.formValues;
+    try {
+        const mailchimpResponse = await mailchimp.lists.addListMember("8ed7e238ac", {
+            email_address: formData.email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: formData.firstName,
+                LNAME: formData.lastName,
+            },
+            tags: [yearToTag[formData.year] ?? ""],
+        });
+    
+        if ('id' in mailchimpResponse) {
+            return NextResponse.json({
+                success: true,
+                message: formData.firstName,
+            })
+        }
+
+        return NextResponse.json({
+            success: false,
+            data: mailchimpResponse,
+        });
+    } catch (e: unknown) {
+        const errorData = e.response?.body?.title || e.message || "An unknown error occurred";
+        if (errorData === "Member Exists") {
+            return NextResponse.json({
+                success: false,
+                message: `A member with the email <strong>${formData.email}</strong> is already subscribed`,
+            })
+        }
+        return NextResponse.json({
+            success: false,
+            message: "There was an error adding you to our email list!"   
+        })
+    }
+}
