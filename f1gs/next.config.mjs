f5b04768ba202d20841aee39f8b48a66c1477c89
Kamal RD <imkamaljay@gmail.com>
2024-10-10 00:13:16 -0400

    Hide Secrets

--- f1gs/next.config.mjs
+++ f1gs/next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        MAILCHIMP_API_KEY: "1af1a6f23b8ae431debd2204acf8f5c2-us4",
-        MAILCHIMP_SERVER_PREFIX: "us4",
-    }
 };
 
 export default nextConfig;
